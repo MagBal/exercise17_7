93910bb875e5e4e749285bf92bed42a812fa4245
Magda Balun <magi@op.pl>
2018-01-29 21:36:17 +0100

    Create files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID':'592930693048-jh3oqbn164h0rfa4e0vfaj3jtpn8oig7.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'gC1k6JepgN9YK9f_jOyek1e3',
+	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
